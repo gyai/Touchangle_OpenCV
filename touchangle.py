f3b9b0d421e8ff978302917244987d60583af394
gyai <79432823+gyai@users.noreply.github.com>
2021-04-19 14:45:53 +0900

    完成

--- touchangle.py
+++ touchangle.py
@@ -22,9 +22,11 @@ import cv2
 import re
 import glob
 import numpy as np
+import math
 
 files = sorted(glob.glob("img/*.png"))
 tskdata = np.array([])
+jyoutaiarray =np.array([])
 for f in files: #imageフォルダ下の全画像データ分繰り返す
     
     #全部の画像データを取得+グレースケール化
@@ -61,7 +63,8 @@ for f in files: #imageフォルダ下の全画像データ分繰り返す
             tannjiku = ellipse[1][0]
             jyotai = tannjiku/tyoujiku # 今は比率を表示している。この後指の状態を基準決めて判別する
             tskdata = np.append(tskdata, taskname)
-            tskdata = np.append(tskdata, str(jyotai))  
+            tskdata = np.append(tskdata, str(jyotai))
+            jyoutaiarray = np.append(jyoutaiarray, jyotai)  
             ## tskdataに[task番号,比率,task番号,比率,~~~]で格納されてる ##
 
             # 楕円描画
@@ -71,17 +74,105 @@ for f in files: #imageフォルダ下の全画像データ分繰り返す
    # cv2.imshow(taskname,resimg)
    # cv2.waitKey()
    # np.where(配列名　条件)
-
-index = np.where(tskdata == "1_1task30")
-print(index)
-print(tskdata[index])
-print(tskdata[index[0]+1])
-print(tskdata[index[0]+2])
+#print(tskdata.dtype) # U32型 ['1_1task01' '0.7803787641266638' '1_1task02' ... '0.7158853693844002''5_5task35' '0.7476881623229324']
+#print(jyoutaiarray.dtype)# float64型 [0.78037876 0.78037876 0.9725435  0.61803402 1.         0.81649663 ... 0.64947324 0.64826668 0.67539631 0.65465368 0.71588537 0.74768816]
 
 
+# ほしい情報は、全体の側面率と右下エラーターゲットだけでの側面率
+
+# 関数:全体の側面率(allpercent))
+def allper(percent):
+    bosuu = len(jyoutaiarray)
+    sokumen = 0
+    bettari = 0
+    for a in jyoutaiarray: 
+        if a <= percent:# jyoutaiが基準(percent)より小さいとき＝側面
+            sokumen += 1
+        else:
+            bettari += 1
+    allpercent_sokumen = (sokumen/bosuu)*100
+    allpercent_bettari = (bettari/bosuu)*100
+    print("全体側面比率",allpercent_sokumen)
+    print("全体腹比率",allpercent_bettari)
+
+# 関数:errorターゲットの側面率(tarpercent)
+def tarper(per):
+    tar_array = np.array([])
+    textarr = np.array(["2_1task16",
+    "2_3task12",
+    "2_5task25",
+    "3_1task28",
+    "3_3task01",
+    "3_5task08",
+    "4_1task13",
+    "4_3task22",
+    "4_5task34",
+    "1_1task25",
+    "1_3task28",
+    "1_5task15",
+    "5_1task31",
+    "5_3task26",
+    "5_5task07",
+    "2_1task06",
+    "2_3task06",
+    "2_5task11",
+    "3_1task32",
+    "3_3task29",
+    "3_5task10",
+    "4_1task27",
+    "4_3task32",
+    "4_5task07",
+    "1_1task32",
+    "1_3task34",
+    "1_5task29",
+    "5_1task17",
+    "5_3task17",
+    "5_5task21",
+    "2_1task23",
+    "2_3task10",
+    "2_5task19",
+    "3_1task12",
+    "3_3task26",
+    "3_5task16",
+    "4_1task18",
+    "4_3task24",
+    "4_5task25",
+    "1_1task17",
+    "1_3task24",
+    "1_5task12",
+    "5_1task28",
+    "5_3task29",
+    "5_5task02"])
+
+    for t in textarr: # 右下ターゲット番号格納配列分ループ→tar_arrayに該当ターゲットの比率を配列で取得
+        index = np.where(tskdata == t) # 全体データからターゲット合致indexを取得
+        tarnum = float(tskdata[index[0]+1]) #ターゲットの比率を取得(tarnum)
+        tar_array = np.append(tar_array,tarnum)
+        
+
+    bosuu = len(tar_array)
+    sokumen = 0
+    bettari = 0
+    for tar in tar_array: 
+        if tar <= per:# jyoutaiが基準(percent)より小さいとき＝側面
+            sokumen += 1
+        else:
+            bettari += 1
+    tarpercent_sokumen = (sokumen/bosuu)*100
+    tarpercent_bettari = (bettari/bosuu)*100
+    print("ターゲット側面比率",tarpercent_sokumen)
+    print("ターゲット腹比率",tarpercent_bettari)
+    
+
+allper(0.5)
+tarper(0.5)
+
+
+'''
 
 
 
 
 
+'''
  
